e9f13fb3fda639be0c3253a51da31d5297284317
Anıl <6429176+senocak@users.noreply.github.com>
2022-10-24 15:17:06 +0300

    Update UserController.java

--- src/main/java/com/github/senocak/clinked/controller/UserController.java
+++ src/main/java/com/github/senocak/clinked/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(UserController.URL)
+// updated controller
 public class UserController {
     public static final String URL = "/api/v1/user";
     private final UserService userService;
